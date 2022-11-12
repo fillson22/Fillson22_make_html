97100cf250d22a6b7d8d66a0518d009e5eca016f
fillson22 <fillson22051983@gmail.com>
2022-11-12 21:39:21 +0200

    111

--- fillson22_make_html.gemspec
+++ fillson22_make_html.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name        = 'fillson22_make_html'
-    s.version     = '0.2.0'
+    s.version     = '0.4.0'
     s.licenses    = ['MIT']
     s.summary     = "This gem great html-code with parametres of my file *.rb"
     s.authors     = ["Iliya Filippov"]
--- lib/fillson22_make_html.rb
+++ lib/fillson22_make_html.rb
@@ -1,25 +1,23 @@
-def greate_file(file_name = 'index', bypass_html = true)
-    html_code= <<~HTML
-    <!DOCTYPE html>
-        <html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <title>Тамагочи</title>
-    </head>
-    <body>
-        <h1>Имя - #{@name}</h1>
-        <p> Сыт - #{@full} </p>
-        <p> Усталость - #{@fatigue}</p>
-        <p> Возраст - #{@age} </p>
-        <p> Здоровье - #{@health} </p>
-        <div style="font-size:5rem;width:100%;text-align:center;">
-        #{emodzi}
-        </div>
-        
-    </body>
-    </html>
-    HTML
-    
-    fileHtml = File.expand_path("#{file_name}.html")
-    File.open(fileHtml, 'w') { |f| f.write html_code }
+require "sanitize"
+
+class PageController
+    def self.greate(body, name: 'index.html', bypass_html: false)
+      body = Sanitize.fragment(body) unless bypass_html
+      File.open(name, "w+") do |code|
+        html_code = <<~HTML
+        <!DOCTYPE html>
+          <html lang="ru">
+        <head>
+          <meta charset="UTF-8">
+          <title>Тамагочи</title>
+        </head>
+        <body>
+          #{body}
+          </div>
+        </body>
+        </html>
+        HTML
+            code.write(html_code)
+      end
+    end
 end
